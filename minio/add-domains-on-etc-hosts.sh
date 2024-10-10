f03a4f5ca16da8517e99d2017242e0e60eeb88ab
Ricardo Bernardes dos Santos <ricardob@stj.jus.br>
2024-10-10 19:18:13 -0300

    refactor

--- minio/add-domains-on-etc-hosts.sh
+++ minio/add-domains-on-etc-hosts.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-#    add-domains-on-etc-hosts.sh
+# add-domains-on-etc-hosts.sh
 
 set -e
 
 IP="127.0.0.1"
 
-DOMAIN="docker.local"
-if ! grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    echo "$IP $DOMAIN" | tee -a /etc/hosts > /dev/null
-    echo "Domínio $DOMAIN adicionado ao /etc/hosts."
-else
-    echo "O domínio $DOMAIN já está presente no /etc/hosts."
-fi
+add_domain_to_hosts() {
+    local DOMAIN=$1
+    if ! grep -q "^$IP $DOMAIN$" /etc/hosts; then
+        echo "$IP $DOMAIN" | tee -a /etc/hosts > /dev/null
+        echo "Domínio $DOMAIN adicionado ao /etc/hosts."
+    else
+        echo "O domínio $DOMAIN já está presente no /etc/hosts."
+    fi
+}
 
-DOMAIN="minio.docker.local"
-if ! grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    echo "$IP $DOMAIN" | tee -a /etc/hosts > /dev/null
-    echo "Domínio $DOMAIN adicionado ao /etc/hosts."
-else
-    echo "O domínio $DOMAIN já está presente no /etc/hosts."
-fi
-
-DOMAIN="s3.minio.docker.local"
-if ! grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    echo "$IP $DOMAIN" | tee -a /etc/hosts > /dev/null
-    echo "Domínio $DOMAIN adicionado ao /etc/hosts."
-else
-    echo "O domínio $DOMAIN já está presente no /etc/hosts."
-fi
+# Domínios que você quer adicionar
+add_domain_to_hosts "docker.local"
+add_domain_to_hosts "minio.docker.local"
+add_domain_to_hosts "s3.minio.docker.local"
--- minio/remove-domains-on-etc-hosts.sh
+++ minio/remove-domains-on-etc-hosts.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-#    remove-domains-on-etc-hosts.sh
+# remove-domains-on-etc-hosts.sh
 
 set -e
 
 IP="127.0.0.1"
 
-DOMAIN="docker.local"
-if grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    sed -i "/^$IP $DOMAIN$/d" /etc/hosts
-    echo "Domínio $DOMAIN removido do /etc/hosts."
-else
-    echo "O domínio $DOMAIN não está presente no /etc/hosts."
-fi
+remove_domain_from_hosts() {
+    local DOMAIN=$1
+    if grep -q "^$IP $DOMAIN$" /etc/hosts; then
+        sed -i "/^$IP $DOMAIN$/d" /etc/hosts
+        echo "Domínio $DOMAIN removido do /etc/hosts."
+    else
+        echo "O domínio $DOMAIN não está presente no /etc/hosts."
+    fi
+}
 
-DOMAIN="minio.docker.local"
-if grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    sed -i "/^$IP $DOMAIN$/d" /etc/hosts
-    echo "Domínio $DOMAIN removido do /etc/hosts."
-else
-    echo "O domínio $DOMAIN não está presente no /etc/hosts."
-fi
-
-DOMAIN="s3.minio.docker.local"
-if grep -q "^$IP $DOMAIN$" /etc/hosts; then
-    sed -i "/^$IP $DOMAIN$/d" /etc/hosts
-    echo "Domínio $DOMAIN removido do /etc/hosts."
-else
-    echo "O domínio $DOMAIN não está presente no /etc/hosts."
-fi
-
+# Domínios que você quer remover
+remove_domain_from_hosts "docker.local"
+remove_domain_from_hosts "minio.docker.local"
+remove_domain_from_hosts "s3.minio.docker.local"
